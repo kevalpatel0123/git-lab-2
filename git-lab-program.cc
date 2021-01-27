2763c684ad06b86222fc277ea2321341756e87b6
Keval Patel <kp005919@ohio.edu>
2021-01-26 20:33:54 -0500

    Hi

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
